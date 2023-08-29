a6ec625319e86c5374ceace53fe4d2a21de4c241
Mohamed Abdellah <94642450+Mohamed-Abdellah89@users.noreply.github.com>
2023-08-29 13:47:14 +0300

    Navbar edit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { useState } from 'react';
-import {RiMenu3Line, RiCloseline} from 'react-icons/ri';
+import react, {useState} from 'react';
+
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/GPT-3.svg';
 
 
 const Navbar = () => {
+  //const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -24,6 +26,14 @@ const Navbar = () => {
         <div className='gpt3__navbar-sign'>
           <p>Sign In</p>
           <button type='button'>Sign Up</button>
+        </div>
+    
+        <div className='gpt3__navbar-menu'>
+
+        {/* {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />} */}
+
         </div>
     </div>
   )
