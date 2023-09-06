77056f791ed1fec665fc06f1420ead1f5a4e6136
Mohamed Abdellah <94642450+Mohamed-Abdellah89@users.noreply.github.com>
2023-09-06 15:21:41 +0300

    finshed brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -2,12 +2,34 @@ import React from 'react'
 
 import './brand.css';
 
+import { google, atlassion, dropbox, shopify, slack } from './imports';
 
 
 const Brand = () => {
   return (
-    <div>
-        Brand
+    <div className='gpt3__brand section__padding'>
+
+      <div>
+        <img src={google} alt="google" />
+        
+      </div>
+
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+
+      <div className='gpt3__brand-div-itlassion'>
+        <img src={atlassion} alt="atlassion" />
+      </div>
+
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+        
     </div>
   )
 }
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png';
+import atlassion from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import slack from '../../assets/slack.png';
+import shopify from '../../assets/shopify.png';
+
+export{
+    google,
+    atlassion,
+    dropbox,
+    slack,
+    shopify
+}
